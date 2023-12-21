340ba2315a6c854fc31e97a8ce35b47cbcccb828
Haidarov <nozimjonhaydarov6@gmail.com>
2023-12-21 18:06:00 +0500

    Initial Commit

---
+++ MathGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathGame
+//
+//  Created by Haidarov N on 21/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by Haidarov N on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
