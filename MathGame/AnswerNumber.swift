7997f82246087ef3414ec7f39193d807ef0fa7d2
Haidarov <nozimjonhaydarov6@gmail.com>
2024-10-17 08:48:20 +0500

    update

---
+++ MathGame/AnswerNumber.swift
@@ -0,0 +1,18 @@
+//
+//  AnswerNumber.swift
+//  MathGame
+//
+//  Created by Haidarov N on 21/12/23.
+//
+
+import SwiftUI
+
+struct AnswerNumber: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnswerNumber()
+}
